c1f1564f721a07cdd392520248b60e6c1a56158b
tiennt0212@gmail.com <leulue@example.com>
2021-07-30 02:56:03 -0700

    Completed: Button Component

---
+++ packages/frontend-nextjs/components/Button/button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
+
+export const Button = ({ primary, backgroundColor, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  onClick: undefined,
+};
---
+++ packages/frontend-nextjs/components/Button/button.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Button } from './button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Primary Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  primary: false,
+  label: 'Secondary Button',
+};
